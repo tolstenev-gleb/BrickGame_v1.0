e13da4a1e84f47b4655d3cb4141259bfab386297
Tolstenev Gleb <tolstenev.gleb@mail.ru>
2025-06-21 16:37:13 +0000

    file structure and Makefile

--- src/main.c
+++ src/main.c
@@ -13,6 +13,30 @@
   }
 #define KEY_ESCAPE 27
 
+typedef enum {
+    Start,
+    Pause,
+    Terminate,
+    Left,
+    Right,
+    Up,
+    Down,
+    Action
+} UserAction_t;
+
+typedef struct {
+    int **field;
+    int **next;
+    int score;
+    int high_score;
+    int level;
+    int speed;
+    int pause;
+} GameInfo_t;
+
+void userInput(UserAction_t action, bool hold);
+
+GameInfo_t updateCurrentState();
 
 int main() {
   WIN_INIT();
