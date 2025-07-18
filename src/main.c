87489c0de154816a3131ca7ba9acd9dc0565290e
Tolstenev Gleb <tolstenev.gleb@gmail.com>
2025-07-18 01:10:12 +0000

    Генерация полей готова. Отрисовка пустых полей работает

--- src/main.c
+++ src/main.c
@@ -14,6 +14,11 @@
   }
 #define KEY_ESCAPE 27
 
+#define ROWS 20
+#define COLS 10
+#define FIG_ROWS 4
+#define FIG_COLS 4
+
 typedef enum {
   Start,
   Pause,
@@ -35,36 +40,87 @@ typedef struct {
   int pause;
 } GameInfo_t;
 
+typedef struct {
+  struct {
+    int *row[ROWS];
+    int cell[ROWS][COLS];
+  } field;
+  struct {
+    int *row[FIG_ROWS];
+    int cell[FIG_ROWS][FIG_COLS];
+  } next;
+  UserAction_t action;
+  bool hold;
+} TetrisInfo_t;
 
-GameInfo_t *getInfo() {
-  static GameInfo_t info;
-  return &info;
+void userInput(UserAction_t action, bool hold);
+TetrisInfo_t *getTetrisInfo();
+GameInfo_t *getGameInfo();
+void game();
+void userInput(UserAction_t action, bool hold);
+GameInfo_t updateCurrentState();
+void showState(GameInfo_t info);
+void gameCycle();
+
+TetrisInfo_t *getTetrisInfo() {
+  static TetrisInfo_t *ptr_tetris_info = NULL;
+  static TetrisInfo_t tetris_info = {0};
+  if (ptr_tetris_info == NULL) {
+    for (int i = 0; i < FIG_ROWS; i++) {
+      tetris_info.next.row[i] = tetris_info.next.cell[i];
+    }
+    for (int i = 0; i < ROWS; i++) {
+      tetris_info.field.row[i] = tetris_info.field.cell[i];
+    }
+    ptr_tetris_info = &tetris_info;
+  }
+  return ptr_tetris_info;
+}
+
+GameInfo_t *getGameInfo() {
+  static GameInfo_t *ptr_game_info = NULL;
+  static GameInfo_t game_info;
+  if (ptr_game_info == NULL) {
+    TetrisInfo_t *ptr_tetris_info = getTetrisInfo();
+    game_info.field = (int **)ptr_tetris_info->field.row;
+    game_info.next = (int **)ptr_tetris_info->next.row;
+
+    ptr_game_info = &game_info;
+  }
+  return ptr_game_info;
 }
 
 void game() {
-  GameInfo_t *info = getInfo();
+  GameInfo_t *info = getGameInfo();
   info->level = 1;
 }
 
-void userInput(UserAction_t action, bool hold);
+void userInput(UserAction_t action, bool hold) { game(); }
 
-void userInput(UserAction_t action, bool hold) {
-  if (action == Action) {
-    game();
+GameInfo_t updateCurrentState() { return *getGameInfo(); }
+
+void showState(GameInfo_t info) {
+  int line = 1;
+  mvprintw(line++, 0, "Level: %d", info.level);
+  mvprintw(line++, 0, "Score: %d", info.score);
+  mvprintw(line++, 0, "High score: %d", info.high_score);
+  mvprintw(line++, 0, "Speed: %d", info.speed);
+  mvprintw(line++, 0, "Next:");
+  for (int i = 0; i < FIG_ROWS; i++, line++) {
+    for (int j = 0; j < FIG_COLS; j++) {
+      mvprintw(line, j * 2, "%s", info.next[i][j] ? "[]" : " .");
+    }
+  }
+  mvprintw(line++, 0, "Field:");
+  for (int i = 0; i < ROWS; i++, line++) {
+    for (int j = 0; j < COLS; j++) {
+      mvprintw(line, j * 2, "%s", info.field[i][j] ? "[]" : " .");
+    }
   }
 }
 
-
-GameInfo_t updateCurrentState() {
-  return *getInfo();
-}
-
-void showState(GameInfo_t info) {
-  mvprintw(0, 0, "Level: %d", info.level);
-}
-
-void view() {
-  mvaddstr(0, 0, "Menu");
+void gameCycle() {
+  // mvaddstr(0, 0, "Menu");
   UserAction_t action;
   bool hold;
   GameInfo_t info;
@@ -86,7 +142,7 @@ void view() {
 
 int main() {
   WIN_INIT();
-  view();
+  gameCycle();
   endwin();
   return 0;
 }
